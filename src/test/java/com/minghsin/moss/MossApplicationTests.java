54797721d4cc92395b183f697b46766299b6a276
minghsin <minghsin@foxmail.com>
2021-01-16 18:45:48 +0800

    init commit

---
+++ src/test/java/com/minghsin/moss/MossApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minghsin.moss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MossApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
